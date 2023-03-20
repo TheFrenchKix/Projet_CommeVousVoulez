7c20f937e79b696de82470f3cd4c47112563bd3e
TheFrenchKix <leolobby02@hotmail.fr>
2023-03-20 13:29:27 +0100

    Ajout de la carte google, recap des choix afficher sur la page 'Etape 1'

--- wp-content/plugins/PL/classes/shortcodes/PL_shortcode_formulaire.php
+++ wp-content/plugins/PL/classes/shortcodes/PL_shortcode_formulaire.php
@@ -6,6 +6,8 @@ class PL_shortcode_formulaire {
 
     static function display($atts) {
 
+        global $wpdb;
+
         if (!$_GET['id']){
 
             return "
@@ -34,38 +36,72 @@ class PL_shortcode_formulaire {
 
             $sql =
             "SELECT A.*, 
-            (SELECT C.`nom` FROM `$db_pays` C WHERE C.`id`=A.`idpays` LIMIT 1) AS 'nompays',
-            (SELECT C.`iso` FROM `$db_pays` C WHERE C.`id`=A.`idpays` LIMIT 1) AS 'iso'
+            (SELECT B.`nom` FROM `$db_pays` B WHERE B.`id`=A.`idpays` LIMIT 1) AS 'nompays',
+            (SELECT B.`codeiso` FROM `$db_pays` B WHERE B.`id`=A.`idpays` LIMIT 1) AS 'iso'
             FROM `$db_voeux` A
             WHERE A.`iduser`=$userid";
 
             $result = $wpdb->get_results($sql, 'ARRAY_A');
 
-            return "<script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>
-            <script type='text/javascript'>
-              google.charts.load('current', {
-                'packages':['geochart'],
+            $codes = json_decode(file_get_contents('http://country.io/iso3.json'), true);
+            $names = json_decode(file_get_contents('http://country.io/names.json'), true);
+            $iso3_to_name = array();
+            foreach($codes as $iso2 => $iso3) {
+                $iso3_to_name[$iso3] = $names[$iso2];
+            }
+
+            $listepays = "";
+            $index = 0;
+
+            foreach ($result as $valeur) {
+
+                // var_dump($iso3_to_iso2($valeur['iso']));
+
+                $index += 1;
+        
+                if ($index == count($result))
+                {
+
+                    $listepays .= "['" . $iso3_to_name[$valeur['iso']] . "']";
+
+                }else{
+
+                    $listepays .= "['" . $iso3_to_name[$valeur['iso']] . "'],\n";
+                }
+
+
+            }
+
+            $carte = '';
+
+            $carte .= '<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+            <script type="text/javascript">
+              google.charts.load("current", {
+                "packages":["geochart"],
               });
               google.charts.setOnLoadCallback(drawRegionsMap);
         
               function drawRegionsMap() {
                 var data = google.visualization.arrayToDataTable([
-                  ['Germany', 200],
-                  ['United States', 300],
-                  ['Brazil', 400],
-                  ['Canada', 500],
-                  ['France', 600],
-                  ['RU', 700]
+                  ["Country"],
+                  '. $listepays .'
                 ]);
         
                 var options = {};
         
-                var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+                var chart = new google.visualization.GeoChart(document.getElementById("regions_div"));
         
                 chart.draw(data, options);
               }
             </script>
-            <div id='regions_div' style='width: 900px; height: 500px;'></div>";
+          </head>
+          <body>
+            <h1> Carte récapitulative </h1>
+
+            <div id="regions_div" style="width: 900px; height: 500px;"></div>
+          </body>';
+
+            return $carte;
 
         }
 
